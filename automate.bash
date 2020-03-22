c466aa0e3b9d56113a3dca3009939e6d2f431735
320834 <justinchen1000@gmail.com>
2020-03-22 18:19:18 -0400

    Finish bash script for automation on any POSIX machine

--- automate.bash
+++ automate.bash
@@ -2,8 +2,29 @@
 # A script to automate data collection and data dump for coronashutdown
 # /home/panda/Desktop/Repository/coronashutdown
 
-# echo $PATH
+PATH=$PATH:/home/panda/bin
+
 cd /home/panda/Desktop/Repository/coronashutdown/selenium_src
 node update_pull.js
+node merge-sel.js
+node create-state-csv.js
 
-echo "Hello World"
+cd ..
+
+git add .
+git commit -m "new data"
+git push
+
+#Move files to update map
+cd 
+cp ~/Desktop/Repository/coronashutdown/final_data/counties-cases.geojson ~/Desktop/Repository/Geojson_data
+cp ~/Desktop/Repository/coronashutdown/final_data/state_confirmation_deaths.csv ~/Desktop/Repository/Geojson_data
+
+cd /home/panda/Desktop/Repository/Geojson_data
+
+#Push to geojson_Data repo
+git add .
+git commit -m "new data"
+git push
+
+echo "Finish pulling data"
