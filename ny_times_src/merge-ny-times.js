bb592fc497227abfe9d141a714d2a7c827271e3f
320834 <justinchen1000@gmail.com>
2020-04-02 19:51:29 -0400

    refractor code, added comments, add function in merge-ny-times for getting date

--- ny_times_src/merge-ny-times.js
+++ ny_times_src/merge-ny-times.js
@@ -19,8 +19,46 @@ let county_dict = {}
 
 let debug_list = [];
 
-let list_population_per_county = {}
+let list_population_per_county_dict = {}
 
+
+//====================================================================================
+//Misc Functions
+
+/**
+ * Takes a date object and format to standard
+ * Standard Definition:
+ * dd.mm.yyyy
+ * @param {Date} date_obj 
+ */
+function format_date(date_obj)
+{
+    let date = "";
+    let month = parseInt(date_obj.getMonth()) + 1;
+    let year = date_obj.getFullYear();
+
+    if (parseInt(date_obj.getDate()) < 10) {
+        date = "0" + date_obj.getDate();
+    } else {
+        date = date_obj.getDate();
+    }
+
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    //In format dd.mm.yyyy
+    let key = date + "." + month + "." + year;
+
+    return key;
+}
+//====================================================================================
+//Part 1 methods
+
+/**
+ * Take county object from geojson format and append date properties
+ * @param {Number} county_index 
+ */
 function construct_dates(county_index) {
     let date_dict = {};
   
@@ -32,22 +70,8 @@ function construct_dates(county_index) {
     for (var i = 0; i < days; i++) {
       millitime = start_date.getTime() + 86400000 * i;
       dateObj = new Date(millitime);
-  
-      let date = "";
-      let month = parseInt(dateObj.getMonth()) + 1;
-      let year = dateObj.getFullYear();
-  
-      if (parseInt(dateObj.getDate()) < 10) {
-        date = "0" + dateObj.getDate();
-      } else {
-        date = dateObj.getDate();
-      }
-  
-      if (month < 10) {
-        month = "0" + month;
-      }
-  
-      let key = date + "." + month + "." + year;
+
+      let key = format_date(dateObj)
       let value = 0;
       date_dict[key] = value;
   
@@ -56,25 +80,40 @@ function construct_dates(county_index) {
   
     return date_dict;
 }
-  
+
+/**
+ * Construct a dictionary of counties. For each county entry, there exists a geojson object that includes
+ * properties of the county and the geometry for the map.
+ * 
+ * Dictionary object => county_dict
+ * key = county_name|state_name
+ * value = The geojson for a specific object
+ */
 function construct_county_dict() {
     for (let i = 0; i < jsonCounties["features"].length; i++) {
         jsonCounties["features"][i]["properties"]["total_cases"] = 0;
         jsonCounties["features"][i]["properties"]["date_ind"] = construct_dates(i);
-        // construct_dates(i);
-        // jsonCounties["features"][i]["properties"]["date_cumul"] = construct_dates();
-        // construct_dates();
 
         let key =
         jsonCounties["features"][i]["properties"]["COUNTY"] +
         "|" +
         jsonCounties["features"][i]["properties"]["STATE"];
+
         let value = jsonCounties["features"][i];
 
         county_dict[key] = value;
     }
 }
 
+//=====================================================================================
+//Part 2 methods
+
+/**
+ * Formats date and place it into the object of the cases
+ * 
+ * TODO: Figure out where this is called and use format_date() instead
+ * @param {Object} obj 
+ */
 function set_date(obj)
 {
     let dateObj = new Date(obj["date"]);
@@ -99,6 +138,10 @@ function set_date(obj)
     obj["date"] = key;
 }
 
+/**
+ * Takes the object and remove the word County from the county property. The dictionary does not have use County
+ * @param {Object} obj 
+ */
 function remove_county(obj)
 {
     if(obj["county"].includes("County"))
@@ -107,18 +150,28 @@ function remove_county(obj)
     }
 }
 
+/**
+ * Loop through cases and formats the cases. The cases are stored in jsonCases global variable
+ */
 function parse_cases()
 {
     for(let i = 0; i < jsonCases.length; i++)
     {
-        // get_number_cases(jsonCases[i]);
-        // stateAbb_to_state(jsonCases[i]);
         set_date(jsonCases[i]);
         remove_county(jsonCases[i]);
-        // console.log(jsonCases[i]["cases"] + " " + jsonCases[i]["state"] + " " + jsonCases[i]["date"]);
     }
 }
 
+//=============================================================================================
+
+/**
+ * Take a case object and check if it falls under the exceptions category
+ * If the case are in specific areas, they are exceptions and have to be handled
+ * 
+ * Ex.
+ * Kansas City should map to Jackson, Clay, Platte, and Cass
+ * @param {Object} obj 
+ */
 function handle_exceptions(obj)
 {
     if(obj["state"] === "District of Columbia")
@@ -139,6 +192,11 @@ function handle_exceptions(obj)
     }
 }
 
+/**
+ * Take case object and append cases to a specific county
+ * @param {Object} caseObj 
+ * @param {Object} countyObj 
+ */
 function append_cases_to_county_dates(caseObj, countyObj)
 {
     let date_confirmation = caseObj["date"];
@@ -149,6 +207,9 @@ function append_cases_to_county_dates(caseObj, countyObj)
 
 }
 
+/**
+ * Cases from NYTimes data and append data to geojson format for map. Geojson will be exported to site
+ */
 function merge_cases_county()
 {
     for(let i = 0; i < jsonCases.length; i++)
@@ -157,28 +218,35 @@ function merge_cases_county()
 
         if(obj["county"] === "" || obj["cases"] === "Death" || obj["state"] === undefined || obj["county"] === "Unassigned" || obj["county"] === "Unknown")
         {
-            //Do nothing these cases do not matter
+            //Do nothing these cases do not matter.
+            //Either the cases does not have state or state or is a death
         }
         else
         {
-            let county_state = obj["county"] + "|" + obj["state"];
+            let county_state_key = obj["county"] + "|" + obj["state"];
 
-            if(county_dict[county_state] !== undefined && county_state !== "New York City|New York")
+            //True if key can be found in dictionary, then add cases to that specific county and date
+            //False if key cannot be found in dictionary and the case is New York City
+            //New York City is different as it encompasses 5 different counties. 
+            if(county_dict[county_state_key] !== undefined && county_state_key !== "New York City|New York")
             {
                 //Append cases for dates
-                append_cases_to_county_dates(obj, county_dict[county_state])
+                append_cases_to_county_dates(obj, county_dict[county_state_key])
             }
             else
             {
+                //If key cannot be found, then look through exceptions list. 
+                //These exceptions are for cities where the cases reported are designated to cities but not specific counties
                 let list = handle_exceptions(obj);
 
+                //Loop through exception list as some cities occupy multiply counties
                 for(let i = 0; i < list.length; i++)
                 {
-                    county_state = list[i] + "|" + obj["state"];
+                    county_state_key = list[i] + "|" + obj["state"];
 
-                    if(county_dict[county_state] !== undefined)
+                    if(county_dict[county_state_key] !== undefined)
                     {
-                        append_cases_to_county_dates(obj, county_dict[county_state])
+                        append_cases_to_county_dates(obj, county_dict[county_state_key])
                     }
                     else
                     {
@@ -192,6 +260,39 @@ function merge_cases_county()
     }
 }
 
+//==================================================================================================
+
+/**
+ * Does error checking on any dates that don't have cases. Takes previous date case if 0 are found
+ */
+function fix_no_cases_date()
+{
+    for (let [key, value] of Object.entries(county_dict)) {
+        // let cumulative_count = 0;
+        let days = Math.floor((end_date - start_date) / 1000 / 60 / 60 / 24);
+
+        for (let i = 1; i < days; i++) {
+            let millitime = start_date.getTime() + 86400000 * i;
+
+            let dateObj = new Date(millitime);
+            let yestObj = new Date(start_date.getTime() + 86400000 * (i-1))
+
+            let displayDate = format_date(dateObj)
+            let yesDisplayDate = format_date(yestObj)
+
+            if(value["properties"][displayDate] == 0)
+            {
+                value["properties"][displayDate] = value["properties"][yesDisplayDate]
+                value["properties"]["date_ind"][displayDate] = value["properties"]["date_ind"][yesDisplayDate]
+            }
+
+        }
+    }
+}
+
+/**
+ * Write cumulative cases to counties-cases.geojson
+ */
 function write_file_cases() {
     let features = [];
   
@@ -206,64 +307,18 @@ function write_file_cases() {
   
     let data = JSON.stringify(jsonWrite);
   
-    fs.writeFileSync("../debug_data/counties-cases.json", data);
+    // fs.writeFileSync("../debug_data/counties-cases.json", data);
     fs.writeFileSync("../final_data/counties-cases.geojson", data);
 }
 
-function fix_no_cases_date()
-{
-    for (let [key, value] of Object.entries(county_dict)) {
-        // let cumulative_count = 0;
-        let days = Math.floor((end_date - start_date) / 1000 / 60 / 60 / 24);
-
-        for (let i = 1; i < days; i++) {
-            let millitime = start_date.getTime() + 86400000 * i;
-
-            let dateObj = new Date(millitime);
-            let yestObj = new Date(start_date.getTime() + 86400000 * (i-1))
-
-            let date = "";
-            let month = parseInt(dateObj.getMonth()) + 1;
-            let year = dateObj.getFullYear();
-
-            let yesDate = "";
-            let yesMonth = parseInt(yestObj.getMonth()) + 1;
-            let yesYear = yestObj.getFullYear();
-
-            if (parseInt(dateObj.getDate()) < 10) {
-                date = "0" + dateObj.getDate();
-            } else {
-                date = dateObj.getDate();
-            }
-
-            if (parseInt(yestObj.getDate()) < 10) {
-                yesDate = "0" + yestObj.getDate();
-            } else {
-                yesDate = yestObj.getDate();
-            }
-
-
-            if (month < 10) {
-                month= "0" + month;
-            }
-
-            if (yesMonth < 10) {
-                yesMonth = "0" + yesMonth;
-            }
-
-            let displayDate = date + "." + month + "." + year;
-            let yesDisplayDate = yesDate + "." + yesMonth + "." + yesYear;
-
-            if(value["properties"][displayDate] == 0)
-            {
-                value["properties"][displayDate] = value["properties"][yesDisplayDate]
-            }
-
-        }
-    }
-}
-
-//=============================================================================================
+//===================================================================================================
+/**
+ * Takes a json object and create a dictionary for county population
+ * 
+ * Stores results in list_population_per_county_dict
+ * 
+ * @param {JSON} jsonObj 
+ */
 function construct_population_county_list(jsonObj)
 {
     // console.log(jsonObj[1])
@@ -291,12 +346,17 @@ function construct_population_county_list(jsonObj)
             }
 
             // console.log(county_name)
-            list_population_per_county[key] = obj
+            list_population_per_county_dict[key] = obj
         }
     }
 }
 
-
+/**
+ * Calculate cases per every 10,000 population in a given county
+ * @param {String} county_state_key 
+ * @param {Number} cases 
+ * @param {Number} population 
+ */
 function get_ratio_per_ten_thousand(county_state_key, cases, population)
 {
     let population_per_thousand = population/10000;
@@ -304,6 +364,16 @@ function get_ratio_per_ten_thousand(county_state_key, cases, population)
 
 }
 
+/**
+ * Handles exceptions for specific cities
+ * 
+ * The ratio for NYC will be the same for counties, Richmond, Kings, Queens, New York, and Bronx
+ * The reason is due to the fact those 5 counties are constructed with the same cases
+ * 
+ * @param {String} county_state_key 
+ * @param {Number} cases 
+ * @param {Number} ratio 
+ */
 function handle_special_cases_population(county_state_key, cases, ratio)
 {
     if(county_state_key === "Kings|New York" 
@@ -320,6 +390,9 @@ function handle_special_cases_population(county_state_key, cases, ratio)
     return ratio
 }
 
+/**
+ * Loop through each county and each date and calculate the cases per ten thousand capita.
+ */
 function calulate_cases_per_capita()
 {
     // let index = 0;
@@ -329,49 +402,27 @@ function calulate_cases_per_capita()
         // let cumulative_count = 0;
         let days = Math.floor((end_date - start_date) / 1000 / 60 / 60 / 24);
 
-        // if(index === 2)
-        // {
-        //     console.log(value);
-        // }
-
-        // index++;
-
         for (let i = 0; i < days; i++) {
             let millitime = start_date.getTime() + 86400000 * i;
 
             let dateObj = new Date(millitime);
 
-            let date = "";
-            let month = parseInt(dateObj.getMonth()) + 1;
-            let year = dateObj.getFullYear();
-
-            if (parseInt(dateObj.getDate()) < 10) {
-                date = "0" + dateObj.getDate();
-            } else {
-                date = dateObj.getDate();
-            }
-
-            if (month < 10) {
-                month= "0" + month;
-            }
-
-
-            let displayDate = date + "." + month + "." + year;
+            let displayDate = format_date(dateObj);
 
             let county_state_key = value["properties"]["COUNTY"] + "|" + value["properties"]["STATE"];
 
-            if(list_population_per_county[county_state_key] != undefined)
+            if(list_population_per_county_dict[county_state_key] != undefined)
             {
-                let population = list_population_per_county[county_state_key]["POPESTIMATE2019"];
-                let cases = value["properties"][displayDate];
+                let population = list_population_per_county_dict[county_state_key]["POPESTIMATE2019"];
+                let cases = value["properties"]["date_ind"][displayDate];
 
-                let cases_per_thousand = get_ratio_per_ten_thousand(county_state_key,cases, population)
-                cases_per_thousand = handle_special_cases_population(county_state_key, cases, cases_per_thousand);
+                let cases_per_ten_thousand = get_ratio_per_ten_thousand(county_state_key,cases, population)
+                cases_per_ten_thousand = handle_special_cases_population(county_state_key, cases, cases_per_ten_thousand);
 
-                cases_per_thousand = cases_per_thousand.toFixed(0);
-                cases_per_thousand = parseInt(cases_per_thousand);
+                cases_per_ten_thousand = cases_per_ten_thousand.toFixed(2);
+                cases_per_ten_thousand = parseFloat(cases_per_ten_thousand);
 
-                value["properties"][displayDate] = cases_per_thousand;
+                value["properties"][displayDate] = cases_per_ten_thousand;
             }
             else
             {
@@ -394,6 +445,9 @@ function calulate_cases_per_capita()
     // console.log(visited)
 }
 
+/**
+ * Main function to get the county of cases per ten thousand capita. First it fetches the data from csv
+ */
 function cases_per_capita()
 {
     // console.log(typeof rawPopulation.toString())
@@ -405,16 +459,13 @@ function cases_per_capita()
 
             calulate_cases_per_capita();
 
-            // console.log(county_dict["New York|New York"]["properties"]["25.03.2020"])
-            // console.log(county_dict["Queens|New York"]["properties"]["25.03.2020"])
-            // console.log(county_dict["Richmond|New York"]["properties"]["25.03.2020"])
-            // console.log(county_dict["Kings|New York"]["properties"]["25.03.2020"])
-            // console.log(county_dict["Bronx|New York"]["properties"]["25.03.2020"])
-
             write_file_cases_per_capita();
         })
 }
 
+/**
+ * Write the county of cases per ten thousand capita to file
+ */
 function write_file_cases_per_capita()
 {
     let features = [];
@@ -434,18 +485,26 @@ function write_file_cases_per_capita()
     fs.writeFileSync("../final_data/counties-per-capita-cases.geojson", data);
 }
 
+//================================================================================================
+
 function main()
 {
+    //Part 1, construct dictionary dict to make it easier to find specific counties
     construct_county_dict();
 
+    //Part 2, parse through cases from NY Times cases and format them.
     parse_cases();
 
+    //Part 3, take the cases from NY Times and merge it to county geojson data. **IMPORTANT STEP**
     merge_cases_county();
 
+    //Part 4, error fix future dates
     fix_no_cases_date()
 
+    //Part 5, write to file
     write_file_cases();
 
+    //Part 6, calculate cases per ten thousand capita.
     cases_per_capita();
 
 }
--- src/test.js
+++ src/test.js
@@ -17,8 +17,34 @@
 
 // console.log(stuff.length);
 
-let f = 10.34
+// let f = 10.34
 
-f = f.toFixed(0)
+// f = f.toFixed(0)
 
-console.log(typeof f)
+// console.log(typeof f)
+
+function format_date(date_obj)
+{
+    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date_obj)
+    const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date_obj)
+    const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date_obj)
+
+    //In format dd.mm.yyyy
+    let date = da + "." + mo + "." + ye;
+
+    return date;
+}
+
+let days = 95;
+let start_date = new Date("01/01/2020");
+
+for(let i = 0; i < days; i++)
+{
+    let millitime = start_date.getTime() + 86400000 * i;
+    let date_obj = new Date(millitime);
+
+    let key = format_date(date_obj);
+
+    console.log(key)
+
+}   
